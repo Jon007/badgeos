4d2670168bbf65c28aa6c86bd9ebcee57b2490d0
Jonathan Moore <Jonathan.Moore@bcs.org>
2021-02-13 14:37:51 +0000

    contextual_help filter deprecated

--- includes/p2p/scb/AdminPage.php
+++ includes/p2p/scb/AdminPage.php
@@ -121,7 +121,8 @@ abstract class scbAdminPage {
 		}
 
 		add_action( 'admin_menu', array( $this, 'page_init' ), $this->args['admin_action_priority'] );
-		add_filter( 'contextual_help', array( $this, '_contextual_help' ), 10, 2 );
+        //JM2021: deprecated and not needed
+		//add_filter( 'contextual_help', array( $this, '_contextual_help' ), 10, 2 );
 
 		if ( $file ) {
 			$this->file = $file;
